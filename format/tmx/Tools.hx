6470bb8e72cb846e0a46faf04e7fd7c468ebaab0
Yanrishatum <krabanek@gmail.com>
2015-05-08 21:21:00 +0300

    Some changes in Tools.hx.

--- format/tmx/Tools.hx
+++ format/tmx/Tools.hx
@@ -30,12 +30,20 @@ class Tools
   public static function getTilesetByGid(map:TmxMap, gid:Int):TmxTileset
   {
     if (gid <= 0) return null; // None
-    var i:Int = map.tilesets.length;
-    while (--i >= 0)
+    var i:Int = 0;
+    while (i < map.tilesets.length)
     {
-      if (map.tilesets[i].firstGID >= gid) return map.tilesets[i];
+      if (map.tilesets[i].firstGID > gid) return map.tilesets[i - 1];
+      i++;
     }
     return null;
+  }
+  
+  public static function getTileUVByLidUnsafe(tileset:TmxTileset, localId:Int, output:Dynamic):Void
+  {
+    var tilesInLine:Int = Math.floor(tileset.image.width / tileset.tileWidth);
+    Reflect.setProperty(output, "x", (localId % tilesInLine) * tileset.tileWidth);
+    Reflect.setProperty(output, "y", Math.ffloor(localId / tilesInLine) * tileset.tileHeight);
   }
   
 }
